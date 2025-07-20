e7fcde651ea8b02c55015284a52c70d2b74de368
aisinast <popnastya6@icloud.com>
2025-07-20 20:59:57 +0300

    Реализованы экран создания задания и ViewModel

---
+++ MindMates/MindMates/Home Tasks/New Task/NewTaskView.swift
@@ -0,0 +1,168 @@
+//
+//  NewTaskView.swift
+//  MindMates
+//
+//  Created by Анастасия on 19.07.2025.
+//
+
+import SwiftUI
+
+struct NewTaskView: View {
+    @Binding var isPresented: Bool
+    
+    @State private var subject : String = ""
+    @State private var teacherId : String = ""
+    @State private var studentId : String = ""
+    @State private var description: String = ""
+    @State private var deadline : Date = Date()
+    @State private var studentEmail : String = ""
+    
+
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.4)
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture {
+                    isPresented = false
+                }
+            VStack {
+                VStack {
+                    Text("Добавить новое домашнее задание")
+                        .foregroundStyle(Color.foreground)
+                        .font(.title)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    ScrollView {
+                        VStack (alignment: .leading, spacing: 8) {
+                            Text("Предмет")
+                                .font(.title3)
+                                .foregroundStyle(Color.secondary)
+                                .bold()
+                            
+                            Picker("Выберите предмет из списка", selection: $subject) {
+                                Text("Математика").tag(1)
+                                Text("Русский").tag(2)
+                                Text("Физика").tag(3)
+                                Text("Английский язык").tag(4)
+                                Text("Биология").tag(5)
+                                Text("Химия").tag(6)
+                                Text("История").tag(7)
+                                Text("География").tag(8)
+                                Text("Литература").tag(9)
+                                Text("Информатика").tag(10)
+                            }
+                            .pickerStyle(.menu)
+                            .tint(Color.foreground)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 44)
+                            .overlay {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .fill(Color.white.opacity(0.5))
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .stroke(Color.foreground, lineWidth: 2)
+                                    }
+                            }
+                            
+                        }
+                        .padding(.horizontal, 10)
+                        
+                        Spacer().frame(height: 20)
+                        
+                        VStack (alignment: .leading, spacing: 8) {
+                            Text("Введите почту ученика")
+                                .font(.title3)
+                                .foregroundStyle(Color.secondary)
+                                .bold()
+                            
+                            TextEditor(text: $studentEmail)
+                                .scrollContentBackground(.hidden)
+                                .foregroundColor(Color.foreground)
+                                .tint(Color.foreground)
+                                .background(Color.white.opacity(0.5))
+                                .frame(height: 44)
+                                .cornerRadius(5)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .stroke(Color.foreground, lineWidth: 2)
+                                }
+                        }
+                        .padding(.horizontal, 10)
+                        
+                        Spacer().frame(height: 20)
+                        
+                        VStack (alignment: .leading, spacing: 8) {
+                            Text("Введите описание задания или прикрепите ссылку")
+                                .font(.title3)
+                                .foregroundStyle(Color.secondary)
+                                .bold()
+                            
+                            TextEditor(text: $description)
+                                .scrollContentBackground(.hidden)
+                                .foregroundColor(Color.foreground)
+                                .tint(Color.foreground)
+                                .background(Color.white.opacity(0.5))
+                                .frame(minHeight: 100)
+                                .cornerRadius(5)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .stroke(Color.foreground, lineWidth: 2)
+                                }
+                        }
+                        .padding(.horizontal, 10)
+                        
+                        Spacer().frame(height: 20)
+                        
+                        VStack (alignment: .leading, spacing: 8) {
+                            Text("Дедлайн")
+                                .font(.title3)
+                                .foregroundStyle(Color.secondary)
+                                .bold()
+                            
+                            DatePicker(
+                                "Выберите дату",
+                                selection: $deadline,
+                                displayedComponents: .date
+                            )
+                            .datePickerStyle(.graphical)
+                            .tint(Color.foreground)
+                            .background(Color.white.opacity(0.5))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(Color.foreground, lineWidth: 2)
+                            }
+                        }
+                        .padding(.horizontal, 10)
+                        
+                        Spacer().frame(height: 30)
+                        
+                        VStack(alignment: .leading, spacing: 20) {
+                            Button {
+                                
+                            } label: {
+                                Text("Создать")
+                                    .font(.title3)
+                                    .padding()
+                                    .background(Color.foreground)
+                                    .foregroundColor(Color.background)
+                                    .cornerRadius(7)
+                            }
+                        }
+                        
+                        Spacer().frame(height: 30)
+                    }
+                }
+            }
+            .frame(width: UIScreen.main.bounds.width - 40)
+            .background(Color.background)
+            .cornerRadius(15)
+            .shadow(radius: 20)
+        }
+    }
+}
+
+#Preview {
+    NewTaskView(isPresented: .constant(true))
+}
